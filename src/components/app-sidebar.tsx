fc25de8ae82495ce5ad3789612d7fea8c4c8bed4
tritk0910 <erizospvp@gmail.com>
2024-11-07 16:42:39 +0700

    Add EN and KO translation to home page

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,62 +1,68 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem
 } from "@/components/ui/sidebar";
-
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-];
+import { getNavItems } from "@/lib/navItem";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { buttonVariants } from "./ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
 
 export function AppSidebar() {
+  const { t } = useTranslation();
+
+  const navItems = getNavItems(t);
+
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+              {navItems.map((navItem, index) => (
+                <Collapsible className="group/collapsible" key={index}>
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton
+                        className={buttonVariants({
+                          variant: "outline",
+                          className: "w-full !justify-start font-normal",
+                        })}
+                      >
+                        {navItem.title}{" "}
+                        <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {navItem.links.map((link, index) => (
+                          <SidebarMenuSubItem
+                            key={index}
+                            className={buttonVariants({
+                              variant: "ghost",
+                              className: "w-full !justify-start font-normal",
+                            })}
+                          >
+                            <Link to={link.url}>{link.title}</Link>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,40 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 import Footer from "../../components/Molecules/Footer";
 import Header from "../../components/Molecules/Header";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
 
 const HomePage = () => {
+  const { t } = useTranslation();
+
+  const descItems = [
+    {
+      title: t("body-1"),
+      desc: t("body-1-detail"),
+    },
+    {
+      title: t("body-2"),
+      desc: t("body-2-detail"),
+    },
+    {
+      title: t("body-3"),
+      desc: t("body-3-detail"),
+    },
+    {
+      title: t("body-4"),
+      desc: t("body-4-detail"),
+    },
+    {
+      title: t("body-5"),
+      desc: t("body-5-detail"),
+    },
+    {
+      title: t("body-6"),
+      desc: t("body-6-detail"),
+    },
+  ];
+
   return (
     <SidebarProvider open={false}>
       <AppSidebar />
@@ -26,27 +56,7 @@ const HomePage = () => {
             <p className="lg:text-[35px] text-xl/[1.4em] tracking-[-.03em] mb-5">
               Our Mission
             </p>
-            <p className="text-base lg:text-xl/[1.5em]">
-              The future of energy management is poised to undergo significant
-              transformations due to advancements in technology, shifting energy
-              demands, and the urgent need to address climate change. Our
-              company explores the emerging trends in energy management,
-              focusing on heat energy management, renewable energy integration,
-              and energy storage solutions. Key innovations include the
-              deployment of artificial intelligence (AI) and machine learning
-              (ML) for predictive analytics, optimizing energy consumption, and
-              reducing operational costs and special Thermal Management Systems.
-              The role of decentralized energy systems, such as microgrids, is
-              also highlighted as a critical driver in the evolution of energy
-              management. Furthermore, the integration of renewable sources like
-              solar, wind, and hydrogen with next-generation storage
-              technologies, including batteries and hydrogen fuel cells, is
-              discussed. Finally, our company addresses the importance of new
-              technology and international cooperation to ensure a sustainable,
-              resilient, and equitable energy future. The convergence of these
-              factors positions energy management as a cornerstone of
-              sustainable development in the 21st century.
-            </p>
+            <p className="text-base lg:text-xl/[1.5em]">{t("our-mission")}</p>
           </div>
           <div className="h-[100px]" />
           <motion.div
@@ -113,13 +123,7 @@ const HomePage = () => {
             transition={{ duration: 1, delay: 0.75 }}
             viewport={{ once: false }}
           >
-            The reason why thermal management companies are important in the
-            renewable energy market is because thermal management plays a key
-            role in improving the efficiency, stability, and economy of
-            renewable energy. Renewable energy is intermittent and irregular, so
-            technology that can store or effectively utilize energy is required.
-            In this context, thermal management technology plays an important
-            role in several aspects.
+            {t("introductory-paragraph")}
           </motion.p>
           <motion.p
             className="text-[15px]/[1.5em]"
@@ -129,101 +133,15 @@ const HomePage = () => {
             viewport={{ once: false }}
           >
             <ul className="list-decimal pl-4 text-pretty">
-              <li>
-                Maximizing energy storage efficiency
-                <p>
-                  Renewable energy, like solar and wind power, produces energy
-                  in varying amounts depending on the time of day. In order to
-                  supply energy stably, energy storage technology is essential,
-                  and this is where thermal storage technology plays an
-                  important role. For example, in solar power generation
-                  systems, the generated thermal energy can be stored in
-                  high-temperature materials and converted into electricity when
-                  needed. This type of thermal storage technology contributes to
-                  maintaining the balance of energy supply and demand in
-                  situations where the supply of renewable energy is irregular.
-                </p>
-              </li>
-              <li>
-                Minimizing energy loss
-                <p>
-                  In order to efficiently utilize renewable energy, the
-                  generated energy must be utilized to the maximum extent
-                  without being wasted. In this case, thermal management plays a
-                  role in minimizing energy loss. By recovering and recycling
-                  waste heat generated from solar panels or wind turbines, the
-                  energy efficiency of the entire system can be increased. In
-                  addition, thermal management devices such as heat exchangers
-                  and cooling systems can optimize equipment performance and
-                  reduce losses in the energy conversion process.
-                </p>
-              </li>
-              <li>
-                Improving the stability of renewable energy systems
-                <p>
-                  Renewable energy power generation facilities can greatly
-                  change their power generation depending on climate conditions,
-                  so effective management of equipment heat is necessary to
-                  operate them stably. For example, solar panels or wind
-                  turbines can accumulate internal heat when operated for long
-                  periods of time, which can lead to equipment performance
-                  degradation or failure. Thermal management systems prevent
-                  such overheating and help equipment operate at optimal
-                  conditions.
-                </p>
-              </li>
-              <li>
-                Reducing carbon emissions
-                <p>
-                  Thermal management technologies contribute to increasing
-                  energy efficiency and reducing carbon emissions across
-                  industries. For example, systems such as **Concentrated Solar
-                  Power (CSP)** concentrate sunlight to produce high-temperature
-                  thermal energy, which is then stored and converted into
-                  electricity when needed. Efficient thermal management in this
-                  process allows more energy to be stored and used, reducing
-                  dependence on fossil fuels. Such thermal management
-                  technologies play an important role in reducing carbon
-                  emissions and responding to climate change.
-                </p>
-              </li>
-              <li>
-                Expanding the application of renewable energy systems in
-                industry and households
-                <p>
-                  To make renewable energy more widely used in homes and
-                  industries, stable supply and cost efficiency are important,
-                  and thermal management technology makes this possible. For
-                  example, a solar heating system for households is designed to
-                  heat water using sunlight, store it, and use it when needed.
-                  This saves energy and makes the use of renewable energy more
-                  attractive.
-                </p>
-              </li>
-              <li>
-                Integrated management of renewable energy and heat
-                <p>
-                  Renewable energy often produces heat as well as electricity.
-                  In particular, solar thermal power generation and geothermal
-                  power generation are methods of directly generating heat, and
-                  if properly managed, they can be used in various fields such
-                  as heating, cooling, and industrial processes in addition to
-                  power generation. Thermal management companies support the
-                  integrated operation of these systems and maximize the
-                  utilization of renewable energy.
-                </p>
-              </li>
+              {descItems.map(({ title, desc }, index) => (
+                <li key={index}>
+                  {title}
+                  <p>{desc}</p>
+                </li>
+              ))}
             </ul>
             <br />
-            <p>
-              In conclusion, thermal management companies play an important role
-              in promoting the efficient production, storage, and utilization of
-              renewable energy, thereby helping to develop the renewable energy
-              market. They are essential partners that contribute to
-              accelerating the energy transition and reducing carbon emissions
-              while reducing energy loss and increasing the stability of the
-              system.
-            </p>
+            <p>{t("conclusion-paragraph")}</p>
           </motion.p>
         </section>
         <Footer />
