1b9209ddf9581dc326a4e4bbb19d4bd7a5f5677a
tientien1103 <le.nttien99@gmail.com>
2024-11-07 15:49:06 +0700

    feat: create products pages

--- src/pages/Utils/RoutesString.ts
+++ src/pages/Utils/RoutesString.ts
@@ -1,6 +1,9 @@
 const RoutesString = {
-  Home: '/',
-  Admin: '/admin'
-}
+  Home: "/",
+  Admin: "/admin",
+  Ess: "/ess-tms",
+  Battery: "/battery-tms",
+  DataCoolingCenter: "/data-cooling-center",
+};
 
-export default RoutesString
+export default RoutesString;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "text-custom": "3px 4px 5px rgba(0, 0, 0, 0.7)",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "16px",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
