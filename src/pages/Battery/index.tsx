3ac371db356b3a52f6e68a75bb0950edb7e29ed5
tientien1103 <le.nttien99@gmail.com>
2024-11-08 11:49:45 +0700

    feat: add motion for products pages

--- src/pages/Battery/index.tsx
+++ src/pages/Battery/index.tsx
@@ -9,6 +9,7 @@ import Header from '@/components/Molecules/Header'
 import Footer from '@/components/Molecules/Footer'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
+import { motion } from "framer-motion";
 
 
 function BatteryPage() {
@@ -18,7 +19,7 @@ function BatteryPage() {
       <AppSidebar />
       <main className='min-h-screen w-full'>
         <Header />
-        <section style={{ backgroundImage: `url(${Banner})` }} className="bg-cover lg:bg-contain bg-no-repeat w-full h-[300px] lg:h-[470px]">
+        <section style={{ backgroundImage: `url(${Banner})` }} className="bg-fixed bg-cover lg:bg-contain bg-no-repeat w-full h-[300px] lg:h-[470px]">
           <div className='flex flex-col items-center justify-center pt-[120px] lg:pt-[150px]'>
             <p className='text-white text-4xl lg:text-[50px] uppercase text-custom'>{t("products.battery_tms.main_path")}</p>
             <div className='flex gap-[15px] mt-[50px]'>
@@ -35,37 +36,53 @@ function BatteryPage() {
           <Container>
             <h1 className='text-[#444] text-center lg:text-start text-[40px] flex flex-col'> <span className='text-[#4972B8]'>{t("products.battery_tms.title")}</span>{t("products.battery_tms.title2")}</h1>
             <hr className='border-t-2 border-[#4972B8] my-5' />
-            <div className='flex gap-5 my-10'>
+            <motion.div className='flex gap-5 my-10'
+              initial={{ translateX: 100, opacity: 0 }}
+              whileInView={{ translateX: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              viewport={{ once: false }}>
               <ChevronRight color='#4972B8' className='w-[45px] h-[45px] self-center' />
               <h3 className='text-[#444] text-[30px]'>{t("products.battery_tms.subtitle")}</h3>
-            </div>
+            </motion.div>
 
             <div className='grid grid-cols-1 lg:grid-cols-3 lg:space-x-[25px] px-4 space-y-6 lg:space-y-0'>
-              <div className='space-y-7'>
+              <motion.div className='space-y-7'
+                initial={{ translateY: 100, opacity: 0 }}
+                whileInView={{ translateY: 0, opacity: 1 }}
+                transition={{ duration: 0.75, delay: 0.1 }}
+                viewport={{ once: false }}>
                 <div className='border border-[#4972B8] w-full'>
                   <p className='text-black text-2xl text-center py-[10px] px-32 lg:px-40'>{t("products.battery_tms.item1.name")}</p>
                 </div>
                 <p className='text-[#444] text-lg'>{t("products.battery_tms.item1.description")}</p>
                 <p className='text-[#444] text-lg'>{t("products.battery_tms.item1.description2")}</p>
-              </div>
+              </motion.div>
 
-              <div className='space-y-7'>
+              <motion.div className='space-y-7'
+                initial={{ translateY: 100, opacity: 0 }}
+                whileInView={{ translateY: 0, opacity: 1 }}
+                transition={{ duration: 0.75, delay: 0.2 }}
+                viewport={{ once: false }}>
                 <div className='border border-[#4972B8]'>
                   <p className='text-black text-2xl text-center py-[10px] px-24 lg:px-32'>{t("products.battery_tms.item2.name")}</p>
                 </div>
                 <p className='text-[#444] text-lg'>{t("products.battery_tms.item2.description")}</p>
                 <p className='text-[#444] text-lg'>{t("products.battery_tms.item2.description2")}</p>
                 <p className='text-[#444] text-lg'>{t("products.battery_tms.item2.description3")}</p>
-              </div>
+              </motion.div>
 
-              <div className='space-y-7'>
+              <motion.div className='space-y-7'
+                initial={{ translateY: 100, opacity: 0 }}
+                whileInView={{ translateY: 0, opacity: 1 }}
+                transition={{ duration: 0.75, delay: 0.3 }}
+                viewport={{ once: false }}>
                 <div className='border border-[#4972B8]'>
                   <p className='text-black text-2xl text-center py-[10px] px-32 lg:px-40'>{t("products.battery_tms.item3.name")}</p>
                 </div>
                 <p className='text-[#444] text-lg'>{t("products.battery_tms.item3.description")}</p>
                 <p className='text-[#444] text-lg'>{t("products.battery_tms.item3.description2")}</p>
                 <p className='text-[#444] text-lg'>{t("products.battery_tms.item3.description3")}</p>
-              </div>
+              </motion.div>
             </div>
             <hr className='border-t-2 border-[#4972B8] my-5' />
           </Container>
@@ -73,32 +90,50 @@ function BatteryPage() {
 
         <section>
           <Container>
-            <div className='flex gap-5 my-10'>
+            <motion.div className='flex gap-5 my-10'
+              initial={{ translateX: 100, opacity: 0 }}
+              whileInView={{ translateX: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              viewport={{ once: false }}>
               <ChevronRight color='#4972B8' className='w-[45px] h-[45px] self-center' />
               <h3 className='text-[#444] text-[30px]'>{t("products.battery_tms.subtitle2")}</h3>
-            </div>
-            <img src={BenefitImg} className='w-full h-[400px] lg:w-[1245px] lg:h-[758px] object-contain' />
+            </motion.div>
+            <motion.img src={BenefitImg} className='w-full h-[400px] lg:w-[1245px] lg:h-[758px] object-contain'
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1 }} />
             <hr className='border-t-2 border-[#4972B8] my-5' />
           </Container>
         </section>
 
         <section>
           <Container>
-            <div className='flex gap-5 my-10'>
+            <motion.div className='flex gap-5 my-10'
+              initial={{ translateX: 100, opacity: 0 }}
+              whileInView={{ translateX: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              viewport={{ once: false }}>
               <ChevronRight color='#4972B8' className='w-[45px] h-[45px] self-center' />
               <h3 className='text-[#444] text-[30px]'>{t("products.battery_tms.subtitle3")}</h3>
-            </div>
-            <img src={ManagementImg} className='w-full h-[400px] lg:w-[1245px] lg:h-[758px] object-contain' />
+            </motion.div>
+            <motion.img src={ManagementImg} className='w-full h-[400px] lg:w-[1245px] lg:h-[758px] object-contain'
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1 }} />
             <hr className='border-t-2 border-[#4972B8] my-5' />
           </Container>
         </section>
 
         <section className="">
           <Container>
-            <div className='flex gap-5 my-10'>
+            <motion.div className='flex gap-5 my-10'
+              initial={{ translateX: 100, opacity: 0 }}
+              whileInView={{ translateX: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              viewport={{ once: false }}>
               <ChevronRight color='#4972B8' className='w-[45px] h-[45px] self-center' />
               <h3 className='text-[#444] text-[30px] text-start'>{t("products.battery_tms.subtitle4")}</h3>
-            </div>
+            </motion.div>
           </Container>
         </section>
 
