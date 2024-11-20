5242151a03bb0bff633142761e7c692a22a36299
Tien Nguyen <tienhg201@gmail.com>
2024-11-20 13:27:10 +0700

    bugfix:20/11 title navigation and map

--- src/pages/AdvanceTechnology/index.tsx
+++ src/pages/AdvanceTechnology/index.tsx
@@ -1,15 +1,25 @@
+import { AppSidebar } from "@/components/app-sidebar";
 import HeaderNavigation from "@/components/Molecules/HeaderNav";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { useBreadcrumb } from "@/hooks/useBreadcrumb";
 import type { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
 
 const AdvanceTechnology: FC = () => {
+  const { t } = useTranslation();
+  const { pathnames } = useBreadcrumb();
+
   return (
-    <div className="w-full">
-      <HeaderNavigation title="Fast Charger Cooling" />
-      <div className="container mt-10">
-        <Outlet />
+    <SidebarProvider open={false}>
+      <AppSidebar />
+      <div className="w-full">
+        <HeaderNavigation title={t(pathnames[1])} />
+        <div className="container mt-10">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 };
 
--- src/pages/BusinessAreas/index.tsx
+++ src/pages/BusinessAreas/index.tsx
@@ -1,10 +1,11 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import Header from "@/components/Molecules/Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { useBreadcrumb } from "@/hooks/useBreadcrumb";
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
 import HeaderNavigation from "../../components/Molecules/HeaderNav";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
-import Footer from "@/components/Molecules/Footer";
-import Header from "@/components/Molecules/Header";
 
 type Props = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,16 +13,18 @@ type Props = {
 };
 
 const BusinessAreas: FC<Props> = () => {
+  const { t } = useTranslation();
+  const { pathnames } = useBreadcrumb();
   return (
     <SidebarProvider open={false}>
       <AppSidebar />
       <div className="w-full">
         <Header />
-        <HeaderNavigation title="Fast Charger Cooling" />
+        <HeaderNavigation title={t(pathnames[1])} />
         <div className="container mt-10">
           <Outlet />
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </SidebarProvider>
   );
